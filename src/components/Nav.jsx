b2fea64fdd4111408fdccc2490b10f5f16a564b6
imoh gabriel <gabrielimoh30@gmail.com>
2023-08-29 17:59:42 +0100

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='d-flex justify-content-between py-5 px-5 navH'>
+        <h2 className='text-white'>Imoh Gabriel</h2>
+        <button className='rounded-pill px-4 py-2'>Contact me</button>
+    </div>
+  )
+}
+
+export default Nav
